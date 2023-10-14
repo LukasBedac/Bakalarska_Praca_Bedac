73565d5735d3ae77a529e14ff3d17844b0d8be6d
Lukas Bedac <lukasbedac222@azet.sk>
2023-10-14 14:06:20 +0200

    Add project files.

---
+++ FRI_Quiz_Bakalarska_Praca/Data/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace FRI_Quiz_Bakalarska_Praca.Data
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
